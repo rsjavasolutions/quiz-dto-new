b1bf7d038a5a7a018eb1a8bacf5863bef9203a13
rsjavasolutions <stan.robert.1986@gmail.com>
2020-06-13 21:36:00 +0200

    Added relations in Question class

--- src/main/java/com/rsjava/quizdto/model/Question.java
+++ src/main/java/com/rsjava/quizdto/model/Question.java
@@ -1,11 +1,15 @@
 package com.rsjava.quizdto.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Data
@@ -17,5 +21,10 @@ public class Question extends EntityBase {
     @NonNull
     private String content;
     @NonNull
+    @OneToMany(mappedBy = "question")
     private List<Answer> answerList;
+    @ManyToOne
+    @JsonIgnore
+    @JoinColumn(name = "test_id")
+    Test test;
 }
